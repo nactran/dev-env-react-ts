d5c012a2ece547e0017dfbf5e7bfb499b15a3c6f
Locke TAN <pleviumtan@gmail.com>
2021-01-21 09:09:47 +0800

    20210121

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
+import keyable from 'Utils/keyable';
 
-interface IProps {
-  name: string;
-  age: number;
-}
+type myState = { date: Date };
+const numbers = [1, 2, 3, 4, 5];
+const listItems: JSX.Element[] = numbers.map(number => <li>{number}</li>);
 
-function App(props: IProps) {
-  const { name, age } = props;
-  return (
-    <div className="app">
-      <span>{`Hello! I'm ${name}, ${age} years old.`}</span>
-    </div>
-  );
+class App extends React.Component<keyable, myState> {
+  timerID: NodeJS.Timeout | undefined;
+  constructor(props: keyable) {
+    super(props);
+    this.state = { date: new Date() };
+    this.timerID = undefined;
+  }
+
+  componentDidMount(): void {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount(): void {
+    if (this.timerID !== undefined) clearInterval(this.timerID);
+  }
+
+  tick(): void {
+    this.setState(() => ({
+      date: new Date(),
+    }));
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-ReactDOM.render(<App name="Locke Tan" age={22} />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
